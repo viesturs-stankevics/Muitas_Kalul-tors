089f0ce420600ed89dd550e9ef1acd223b7514f3
viesturs-stankevics <viesturs.stan@gmail.com>
2022-04-03 23:58:13 +0300

    Update Muitas_Kalkulātors.py

--- Muitas_Kalkulātors.py
+++ Muitas_Kalkulātors.py
@@ -1,7 +1,7 @@
 ﻿from math import*
 from sys import*
 
-def atkārtot():
+def atkārtot1():
     min=int(input("Vai pasūtījuma summa pārsneidz 150 eiro? Jā - 1 Nē - 2  "))
     if min == 1:
         print("Tālāk:")
@@ -10,6 +10,16 @@ def atkārtot():
         exit()
     else:
         print ("Lūdzu izvēlaties vienu no piedāvātajā atbilžu opcijām.")
-        atkārtot()
+        atkārtot1()
 
-atkārtot()
+def atkārtot2():
+    kas=int(input("Vai prece ir taborēta vai adīta? Jā - 1 Nē - 2  " ))
+    if kas == 1:
+        print("Adīts_Tamborēts")
+    elif kas == 2:
+        print("Pārējais")
+    else:
+        print ("Lūdzu izvēlaties vienu no piedāvātajā atbilžu opcijām.")
+        atkārtot2()
+atkārtot1()
+atkārtot2()
