550eaa999b2a6052b0ea1857760c2a76740b6e15
viesturs-stankevics <100201815+viesturs-stankevics@users.noreply.github.com>
2022-04-03 23:27:41 +0300

    Add files via upload

---
+++ Muitas_Kalkulātors.py
@@ -0,0 +1,15 @@
+﻿from math import*
+from sys import*
+
+def atkārtot():
+    min=int(input("Vai pasūtījuma summa pārsneidz 150 eiro? Jā - 1 Nē - 2  "))
+    if min == 1:
+        print("Tālāk:")
+    elif min == 2:
+        print("muita nav jāmaksā")
+        exit()
+    else:
+        print ("Lūdzu izvēlaties vienu no piedāvātajā atbilžu opcijām.")
+        atkārtot()
+
+atkārtot()
